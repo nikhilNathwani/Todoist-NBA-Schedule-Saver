e4345e48c68dcc5ee1a0d142ba9369574adab7fc
nikhilNathwani <nnathwani36@gmail.com>
2024-10-22 08:59:07 -0400

    team picker

--- public/team-picker.js
+++ public/team-picker.js
@@ -1,6 +1,8 @@
 // Function to fetch the NBA schedule and populate the dropdown
 async function populateTeamDropdown() {
+    console.log("In populateTeamDropdown()");
     try {
+        console.log("In try statement");
         // Fetch the nba-schedule.json file
         const response = await fetch('../data/nba-schedule.json');
         console.log("Response", response);
