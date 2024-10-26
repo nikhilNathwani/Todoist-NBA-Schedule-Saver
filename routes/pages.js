f7a9eb2578c4f4c52541212f40237096594387f9
nikhilNathwani <nnathwani36@gmail.com>
2024-10-25 20:33:54 -0400

    todoist brand buttons 1

--- routes/pages.js
+++ routes/pages.js
@@ -87,7 +87,7 @@ router.get("/configure-import", async (req, res) => {
 						<legend><span class="step">2</span> Select Todoist project</legend>
 						${projectPickerHTML}
 					</fieldset>
-					<button type="submit">Import schedule</button>
+					<button class="button" type="submit">Import schedule</button>
 				</form>
 			</body>
 			</html>
