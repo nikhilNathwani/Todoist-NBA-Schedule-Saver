e2911d6a44543c6841c47936e1ec4b5d1b8ec302
nikhilNathwani <nnathwani36@gmail.com>
2024-10-26 09:56:18 -0400

    change nba logo to team logo on team select

---
+++ public/scripts/team-select.js
@@ -0,0 +1,24 @@
+const teamSelect = document.getElementById("team-selector");
+const teamLogo = document.getElementById("nba-logo-container");
+
+// Example team logos
+// const teamLogos = {
+// 	lakers: "lakers-logo.png",
+// 	celtics: "celtics-logo.png",
+// 	// Add more teams and their logo URLs here
+// };
+
+const teamLogos = {
+	ATL: "../public/images/ATL.png",
+};
+
+teamSelect.addEventListener("change", function () {
+	if (teamLogo.id == "nba-logo-container") {
+		teamLogo.id = "team-logo-container";
+	}
+	const selectedTeam = "ATL";
+	// const selectedTeam = teamSelect.value;
+	const teamLogoImg = teamLogo.querySelector("img");
+	teamLogoImg.src = teamLogos[selectedTeam]; // Update logo
+	teamLogoImg.alt = `${selectedTeam} Logo`; // Update alt text for accessibility
+});
