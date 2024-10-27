cd50f30539c6185788b60a76f891c525505557e4
nikhilNathwani <nnathwani36@gmail.com>
2024-10-27 16:48:28 -0400

    dynamic subtitle for new project 120

--- public/scripts/teamSelect.js
+++ public/scripts/teamSelect.js
@@ -56,34 +56,11 @@ const urlParams = new URLSearchParams(window.location.search);
 const isInboxDefault = urlParams.get("isInboxDefault");
 
 teamSelect.addEventListener("change", function () {
-	// if (teamLogo.id == "nba-logo-container") {
-	// 	teamLogo.id = "team-logo-container";
-	// }
 	const selectedTeam = teamSelect.value;
-	newProjectSubtitle.textContent = `Import games into a project called "# ${
-		teamNames[teamSelect.value]
-	} schedule"`;
 
 	const teamLogoImg = teamLogo.querySelector("img");
-	teamLogoImg.classList.add("fade");
+	teamLogoImg.src = "images/team-logos/" + selectedTeam + ".svg";
+	teamLogoImg.alt = `${selectedTeam} Logo`;
 
-	// Wait for the fade to finish, then update the logo and fade back in
-	// setTimeout(() => {
-	// 	teamLogoImg.src = "images/team-logos/" + selectedTeam + ".svg";
-	// 	teamLogoImg.alt = `${selectedTeam} Logo`;
-	// 	teamLogoImg.classList.remove("fade"); // Remove fade-out to trigger fade-in
-	// }, 1000);
-
-	// Wait for the fade-out transition to end before changing the image
-	teamLogoImg.addEventListener("transitionend", function handleTransition() {
-		// Change the logo source
-		teamLogoImg.src = "images/team-logos/" + selectedTeam + ".svg";
-		teamLogoImg.alt = `${selectedTeam} Logo`;
-
-		// Remove fade class to fade in
-		teamLogoImg.classList.remove("fade");
-
-		// Remove the event listener to prevent it from firing multiple times
-		teamLogoImg.removeEventListener("transitionend", handleTransition);
-	});
+	newProjectSubtitle.textContent = `Import games into a project called "# ${teamNames[selectedTeam]} schedule"`;
 });
