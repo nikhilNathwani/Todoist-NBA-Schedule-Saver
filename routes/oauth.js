d762bd0134a1cf102f5fdfa60d68a87a91b48d91
nikhilNathwani <nnathwani36@gmail.com>
2024-11-02 11:53:35 -0400

    get project id (and create new if newProject) 17

--- routes/oauth.js
+++ routes/oauth.js
@@ -16,7 +16,7 @@ const { CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, STATE_SECRET } = process.env;
 
 // Redirect to Todoist for OAuth authorization
 router.get("/login", (req, res) => {
-	const authUrl = `https://todoist.com/oauth/authorize?client_id=${CLIENT_ID}&scope=data:read,data:delete&state=${STATE_SECRET}&redirect_uri=${REDIRECT_URI}`;
+	const authUrl = `https://todoist.com/oauth/authorize?client_id=${CLIENT_ID}&scope=data:read_write&state=${STATE_SECRET}&redirect_uri=${REDIRECT_URI}`;
 	res.redirect(authUrl);
 });
 
