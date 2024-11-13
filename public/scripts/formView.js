e4be26f3e9c131be881a0466c3e235aa5a631a8b
nikhilNathwani <nnathwani36@gmail.com>
2024-11-13 09:59:32 -0500

    post-submit status page

---
+++ public/scripts/formView.js
@@ -0,0 +1,69 @@
+const submitButton = document.getElementById("submitButton");
+const teamLogo = document
+	.getElementById("nbaLogoContainer")
+	.querySelector("img");
+const teamNames = {
+	ATL: "Hawks",
+	BOS: "Celtics",
+	BKN: "Nets",
+	CHA: "Hornets",
+	CHI: "Bulls",
+	CLE: "Cavaliers",
+	DAL: "Mavericks",
+	DEN: "Nuggets",
+	DET: "Pistons",
+	GS: "Warriors",
+	HOU: "Rockets",
+	IND: "Pacers",
+	LAC: "Clippers",
+	LAL: "Lakers",
+	MEM: "Grizzlies",
+	MIA: "Heat",
+	MIL: "Bucks",
+	MIN: "Timberwolves",
+	NO: "Pelicans",
+	NY: "Knicks",
+	OKC: "Thunder",
+	ORL: "Magic",
+	PHI: "Sixers",
+	PHO: "Suns",
+	POR: "Trailblazers",
+	SAC: "Kings",
+	SA: "Spurs",
+	TOR: "Raptors",
+	UTA: "Jazz",
+	WAS: "Wizards",
+};
+const newProjectSubtitle = document
+	.getElementById("newProject")
+	.querySelector("small");
+
+function enableSubmitButton() {
+	submitButton.disabled = false;
+}
+
+function updateTeamSpecificUI(selectedTeam) {
+	updateTeamLogo(selectedTeam);
+	updateNewProjectSubtitle(selectedTeam);
+}
+
+function updateTeamLogo(selectedTeam) {
+	teamLogo.src = "images/team-logos/" + selectedTeam + ".svg";
+	teamLogo.alt = `${selectedTeam} Logo`;
+}
+
+function updateNewProjectSubtitle(selectedTeam) {
+	if (!isInboxDefault()) {
+		newProjectSubtitle.textContent = `Import games into a new Todoist project called "${teamNames[selectedTeam]} schedule"`;
+	}
+}
+
+function isInboxDefault() {
+	const urlParams = new URLSearchParams(window.location.search);
+	return urlParams.get("isInboxDefault") !== "false";
+}
+
+module.exports = {
+	enableSubmitButton,
+	updateTeamSpecificUI,
+};
--- public/scripts/handleInput.js
+++ public/scripts/handleInput.js
@@ -1,25 +1,16 @@
+import { enableSubmitButton, updateTeamSpecificUI } from ("./formView.js");
 const form = document.querySelector("form");
 const teamSelect = form.elements["team"];
 const projectSelect = form.elements["project"];
 
 teamSelect.addEventListener("change", function () {
 	enableSubmitButton();
-	const selectedTeam = teamSelect.value;
-	updateTeamLogo(selectedTeam);
-	updateNewProjectSubtitle(selectedTeam);
+	updateTeamSpecificUI(teamSelect.value);
 });
 
 form.addEventListener("submit", function (event) {
 	event.preventDefault(); // Prevent the form from submitting in the traditional way
-
-	const selectedTeam = teamSelect.value; // Get selected team option
-	const selectedProject = projectSelect.value; // Get selected project option
-
-	// You can still log the selected values for testing
-	console.log("Selected Team:", selectedTeam);
-	console.log("Selected Project:", selectedProject);
-
-	startImport(selectedTeam, selectedProject);
+	startImport(teamSelect.value, projectSelect.value);
 });
 
 function startImport(team, project) {
@@ -33,22 +24,19 @@ function startImport(team, project) {
 		.then((response) => {
 			if (response.ok) {
 				console.log("Import started successfully.");
-				// Start polling the server for the import status
-				showImportStatusUI();
+				showImportStatusUI(importStatus.LOADING);
 				pollStatus();
 			} else {
 				return response.json().then((data) => {
-					console.error("Error starting import:", data.message);
-					showImportStatusUI(
-						`Failed to start import: ${data.message}`
-					);
+					console.error("Error starting import in THEN-ELSE:", data.message);
+					showImportStatusUI(importStatus.ERROR, data.message);
 					// alert(`Failed to start import: ${data.message}`);
 				});
 			}
 		})
 		.catch((error) => {
-			console.error("Error starting import:", error);
-			showImportStatusUI(`Failed to start import: ${error}`);
+			console.error("Error starting import in CATCH:", error);
+			showImportStatusUI(importStatus.ERROR, error);
 		});
 }
 
@@ -68,11 +56,13 @@ function pollStatus() {
 				} else {
 					// document.getElementById("status-message").textContent =
 					// 	"Status: Import complete!";
+					showImportStatusUI(importStatus.SUCCESS);
 					clearInterval(intervalId); // Stop polling once import is complete
 				}
 			})
 			.catch((error) => {
 				console.error("Error checking status:", error);
+				showImportStatusUI(importStatus.ERROR, error);
 				clearInterval(intervalId); // Stop polling on error
 			});
 	}, 3000); // Poll every 3 seconds
@@ -83,63 +73,27 @@ function pollStatus() {
 //           HELPER FUNCTIONS                //
 //                                           //
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
-const submitButton = document.getElementById("submitButton");
-const teamLogo = document
-	.getElementById("nbaLogoContainer")
-	.querySelector("img");
-const teamNames = {
-	ATL: "Hawks",
-	BOS: "Celtics",
-	BKN: "Nets",
-	CHA: "Hornets",
-	CHI: "Bulls",
-	CLE: "Cavaliers",
-	DAL: "Mavericks",
-	DEN: "Nuggets",
-	DET: "Pistons",
-	GS: "Warriors",
-	HOU: "Rockets",
-	IND: "Pacers",
-	LAC: "Clippers",
-	LAL: "Lakers",
-	MEM: "Grizzlies",
-	MIA: "Heat",
-	MIL: "Bucks",
-	MIN: "Timberwolves",
-	NO: "Pelicans",
-	NY: "Knicks",
-	OKC: "Thunder",
-	ORL: "Magic",
-	PHI: "Sixers",
-	PHO: "Suns",
-	POR: "Trailblazers",
-	SAC: "Kings",
-	SA: "Spurs",
-	TOR: "Raptors",
-	UTA: "Jazz",
-	WAS: "Wizards",
-};
-const newProjectSubtitle = document
-	.getElementById("newProject")
-	.querySelector("small");
-
-function enableSubmitButton() {
-	submitButton.disabled = false;
+const importStatus= {
+	LOADING: 0,
+	SUCCESS: 1,
+	ERROR: 2
+}
+function getStatusMessage(status) {
+	switch (status) {
+		case importStatus.LOADING: return "Importing schedule...";
+		case importStatus.SUCCESS: return "Import complete!";
+		case importStatus.ERROR: return "An error occurred";
+		default: return "An error occurred";
+  }
 }
 
-function updateTeamLogo(selectedTeam) {
-	teamLogo.src = "images/team-logos/" + selectedTeam + ".svg";
-	teamLogo.alt = `${selectedTeam} Logo`;
-}
-
-function updateNewProjectSubtitle(selectedTeam) {
-	if (!isInboxDefault()) {
-		newProjectSubtitle.textContent = `Import games into a new Todoist project called "${teamNames[selectedTeam]} schedule"`;
-	}
-}
-function isInboxDefault() {
-	const urlParams = new URLSearchParams(window.location.search);
-	return urlParams.get("isInboxDefault") !== "false";
+function getStatusSubtitle(status, errorMessage=null) {
+	switch (status) {
+		case importStatus.LOADING: return "Please keep this window open";
+		case importStatus.SUCCESS: return "Schedule added to Todoist";
+		case importStatus.ERROR: return `${errorMessage}`;
+		default: return "Unknown error";
+  }
 }
 
 function growLogoBanner() {
@@ -147,24 +101,23 @@ function growLogoBanner() {
 	logoBanner.classList.add("logo-banner-large");
 }
 
-function showLoadingStatus(isImporting, errorMessage) {
+function showLoadingStatus(status, errorMessage) {
 	const status = document.querySelector("h1");
-	status.textContent = isImporting ? "Importing..." : "An error occurred";
+	status.textContent = getStatusMessage(status);
 
 	const subtitle = document.querySelector("h3");
-	subtitle.textContent = isImporting
-		? "Please keep this window open"
-		: errorMessage;
+	subtitle.textContent = getStatusSubtitle(status, errorMessage);
 
 	const statusContainer = document.querySelector(".app-status");
 	statusContainer.classList.add("fade-in");
 }
 
-function showImportStatusUI(isImporting, errorMessage = null) {
+function showImportStatusUI(status, errorMessage = null) {
 	const appFrame = document.querySelector(".app-frame");
 	appFrame.classList.add("loading");
 	appFrame.addEventListener("transitionend", () => {
 		growLogoBanner();
-		showLoadingStatus(isImporting, errorMessage);
+		showLoadingStatus(status, errorMessage);
 	});
 }
+
