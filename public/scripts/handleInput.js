2e59da96393deff7c1259f857dd5dc8ebfdb42e9
nikhilNathwani <nnathwani36@gmail.com>
2024-11-18 13:46:17 -0500

    next steps list 18

--- public/scripts/handleInput.js
+++ public/scripts/handleInput.js
@@ -148,7 +148,7 @@ function showImportStatusUI(status, errorMessage = null) {
 		appContent.appendChild(nextSteps);
 		setTimeout(() => {
 			nextSteps.classList.add("fade-in");
-		}, 1500);
+		}, 1200);
 	}
 }
 
