e2386f1102e135174e088841208411418f55eaf4
nikhilNathwani <nnathwani36@gmail.com>
2024-11-19 10:34:20 -0500

    favicon

--- routes/pages.js
+++ routes/pages.js
@@ -81,6 +81,24 @@ function makeHTMLIntro(pageTitle) {
 				<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 				<title>${pageTitle}</title>
 				<link rel="stylesheet" href="style.css" />
+				<link
+					rel="apple-touch-icon"
+					sizes="180x180"
+					href="/images/favicon/apple-touch-icon.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="32x32"
+					href="/images/favicon/favicon-32x32.png"
+				/>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="16x16"
+					href="/images/favicon/favicon-16x16.png"
+				/>
+				<link rel="manifest" href="/images/favicon/site.webmanifest" />
 				<script
 					src="https://kit.fontawesome.com/caba6ce64c.js"
 					crossorigin="anonymous"
