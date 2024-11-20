72f86a5c73c68db531b4064401174ad7e8eea79e
nikhilNathwani <nnathwani36@gmail.com>
2024-11-20 08:13:01 -0500

    cookie session debugging 2

--- app.js
+++ app.js
@@ -17,9 +17,9 @@ app.use(
 		maxAge: 24 * 60 * 60 * 1000, // 24 hours
 		httpOnly: true, // Prevents client-side JS from accessing the cookie
 		secure: true, // Only HTTPS not HTTP
-		secure: false,
+		// secure: false,
 		// sameSite: "Strict", // Mitigates CSRF attacks
-		sameSite: "Lax",
+		// sameSite: "Lax",
 	})
 );
 
