6044ef170bd7d4661a32b556514ea82db13d5c56
nikhilNathwani <nnathwani36@gmail.com>
2024-11-23 11:16:51 -0500

    fixed title of configure import page

--- routes/pages.js
+++ routes/pages.js
@@ -154,7 +154,7 @@ const htmlIntro = `
 			<meta charset="UTF-8" />
 			<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-			<title>"Select Team and Project Settings"</title>
+			<title>Select Team and Project Settings</title>
 			<link rel="stylesheet" href="style.css" />
 			<link
 				rel="apple-touch-icon"
