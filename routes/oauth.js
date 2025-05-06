d61fdb92e1729404105fbf0f2fe0c9b79fa1c3b7
nikhilNathwani <nnathwani36@gmail.com>
2025-05-06 18:36:37 -0400

    trying res.end() after redirection to configure-import

--- routes/oauth.js
+++ routes/oauth.js
@@ -38,6 +38,7 @@ router.get("/callback", async (req, res) => {
 	const redirectUrlParam = await userReachedProjectLimit(accessToken);
 	console.log("In /oauth, about to redirect to /configure-import now");
 	res.redirect(`/configure-import?isInboxDefault=${redirectUrlParam}`);
+	res.end(); // Force response to flush
 });
 
 module.exports = router;
