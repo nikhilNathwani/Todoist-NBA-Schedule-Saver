f7b4b64b79c6f0edb60303b7d330a0272d09c8a5
nikhilNathwani <nnathwani36@gmail.com>
2025-05-21 08:08:40 -0400

    updating console log in server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-//Server.js only needed for local development & testing
-
 const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 // Start the server
 app.listen(PORT, () => {
-	console.log(`Server running on http://localhost:${PORT}`);
+	if (process.env.NODE_ENV === "production") {
+		console.log(`Server running on port ${PORT}`);
+	} else {
+		console.log(`Server running on http://localhost:${PORT}`);
+	}
 });
